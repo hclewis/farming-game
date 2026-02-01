265cd6efada87cc565ce01b08037438cdac8cf9c
hclewis <hannah@hannahlewis.co.uk>
2026-02-01 15:38:53 +0000

    Adds game timer

--- main.py
+++ main.py
@@ -20,9 +20,14 @@ empty_field_scaled = pygame.transform.scale(empty_field, (300, 300))
 # create button instances
 start_game_button = button.Button(500, 200, start_small, 1)
 
+# timers
+TIMEREVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(TIMEREVENT, 1000) # every 1000 milliseconds = every 1 second
+
 # game variables
 page_state = "menu"
 run = True
+game_timer = 0
 
 while run:
     screen.blit(background_scaled, (0, 0))
@@ -37,6 +42,9 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        elif event.type == TIMEREVENT:
+            game_timer += 1
+            print(f"{game_timer}")
 
     pygame.display.update()
 
